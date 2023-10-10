7b94c39667fd6f1feb633e52bc7347d1babc33eb
Eglė Janušaityė <eglejanusaityte@gmail.com>
2023-10-10 20:28:47 +0300

    Full cruds

--- djangoProject/comparer/views.py
+++ djangoProject/comparer/views.py
@@ -26,6 +26,148 @@ class SongViewSet(viewsets.ModelViewSet):
     serializer_class = SongSerializer
 
 
+#songs/
+@api_view(['GET', 'POST'])
+def handle_song(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            playlist_ids = data.get('playlist')
+            if Playlist.objects.filter(id__in=playlist_ids).count() != len(playlist_ids):
+                return JsonResponse({'success': False, 'error': f'Wrong playlist data'}, status=400)
+            serializer = SongSerializer(data=data)
+
+            if serializer.is_valid():
+                song = serializer.save(playlist=playlist_ids)
+                return JsonResponse({'success': True, 'playlist': serializer.data}, status=201)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'error': 'Invalid JSON data'}, status=400)
+
+    elif request.method == 'GET':
+        try:
+            songs = Song.objects.all()
+            serializer = SongSerializer(songs, many=True)
+            return JsonResponse(serializer.data, safe=False, status=200)
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'error': 'Invalid JSON data'}, status=400)
+    else:
+        return JsonResponse({'error': 'Method not allowed.'}, status=405)
+
+
+@api_view(['GET', 'PATCH', 'DELETE'])
+def handle_song_id(request, pk):
+    try:
+        song = Song.objects.get(pk=pk)
+    except Song.DoesNotExist:
+        return JsonResponse({'success': False, 'error': 'Song not found'}, status=400)
+    except EmptyDataError as e:
+        return JsonResponse({'success': False, 'error': str(e)}, status=400)
+
+    if request.method == 'GET':
+        serializer = SongSerializer(song)
+        return JsonResponse(serializer.data, safe=False, status=200)
+
+    elif request.method == 'DELETE':
+        song.delete()
+        return HttpResponse(status=204)
+
+    elif request.method == 'PATCH':
+        try:
+            data = json.loads(request.body.decode('utf-8'))
+            if not data:
+                return JsonResponse({'success': False, 'error': 'Empty data in PATCH request'}, status=400)
+            if data.get('playlist'):
+                playlist_ids = data.get('playlist')
+                if Playlist.objects.filter(id__in=playlist_ids).count() != len(playlist_ids):
+                    return JsonResponse({'success': False, 'error': f'Wrong playlist data'}, status=400)
+            serializer = SongSerializer(song, data=data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=200)
+            return JsonResponse(serializer.errors, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'error': 'Invalid JSON data'}, status=400)
+    else:
+        return JsonResponse({'error': 'Method not allowed.'}, status=405)
+
+
+#playlists/
+@api_view(['GET', 'POST'])
+def handle_playlist(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            category_id = data.get('category')
+            category = Category.objects.get(id=category_id)
+            data = {
+                'name': request.data.get('name'),
+                'description': request.data.get('description'),
+                'category': category_id,
+            }
+            serializer = PlaylistSerializer(data=data)
+
+            if serializer.is_valid():
+                playlist = serializer.save(category_id=category_id)
+                return JsonResponse({'success': True, 'playlist': serializer.data}, status=201)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'error': 'Invalid JSON data'}, status=400)
+        except Category.DoesNotExist:
+            return JsonResponse({'success': False, 'error': 'Category not found'}, status=400)
+
+    elif request.method == 'GET':
+        try:
+            playlists = Playlist.objects.all()
+            serializer = PlaylistSerializer(playlists, many=True)
+            return JsonResponse(serializer.data, safe=False, status=200)
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'error': 'Invalid JSON data'}, status=400)
+    else:
+        return JsonResponse({'error': 'Method not allowed.'}, status=405)
+
+
+@api_view(['GET', 'PATCH', 'DELETE'])
+def handle_playlist_id(request, pk):
+    try:
+        playlist = Playlist.objects.get(pk=pk)
+    except Playlist.DoesNotExist:
+        return JsonResponse({'success': False, 'error': 'Playlist not found'}, status=400)
+    except EmptyDataError as e:
+        return JsonResponse({'success': False, 'error': str(e)}, status=400)
+
+    if request.method == 'GET':
+        serializer = PlaylistSerializer(playlist)
+        return JsonResponse(serializer.data, safe=False, status=200)
+
+    elif request.method == 'DELETE':
+        playlist.delete()
+        return HttpResponse(status=204)
+
+    elif request.method == 'PATCH':
+        try:
+            data = json.loads(request.body.decode('utf-8'))
+            if not data:
+                return JsonResponse({'success': False, 'error': 'Empty data in PATCH request'}, status=400)
+            if data.get('category'):
+                category_id = data.get('category')
+                category = Category.objects.get(id=category_id)
+            serializer = PlaylistSerializer(playlist, data=data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=200)
+            return JsonResponse(serializer.errors, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'error': 'Invalid JSON data'}, status=400)
+        except Category.DoesNotExist:
+            return JsonResponse({'success': False, 'error': 'Category not found'}, status=400)
+    else:
+        return JsonResponse({'error': 'Method not allowed.'}, status=405)
+
+
+#categories/
 @api_view(['GET', 'POST'])
 def handle_category(request):
     if request.method == 'POST':
@@ -60,7 +202,7 @@ def handle_category_id(request, pk):
     try:
         category = Category.objects.get(pk=pk)
     except Category.DoesNotExist:
-        return HttpResponseNotFound("Category not found")
+        return JsonResponse({'success': False, 'error': 'Category not found'}, status=400)
     except EmptyDataError as e:
         return JsonResponse({'success': False, 'error': str(e)}, status=400)
 
@@ -89,12 +231,13 @@ def handle_category_id(request, pk):
         return JsonResponse({'error': 'Method not allowed.'}, status=405)
 
 
+#categories/(?P<pk>\d+)/playlists/
 @api_view(['GET', 'POST'])
-def handle_playlist(request, pk):
+def handle_playlist_hierarchy(request, pk):
     try:
         category = Category.objects.get(pk=pk)
     except Category.DoesNotExist:
-        return HttpResponseNotFound("Category not found")
+        return JsonResponse({'success': False, 'error': 'Category not found'}, status=400)
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
@@ -120,22 +263,20 @@ def handle_playlist(request, pk):
             return JsonResponse(serializer.data, safe=False, status=200)
         except json.JSONDecodeError:
             return JsonResponse({'success': False, 'error': 'Invalid JSON data'}, status=400)
-        except json.JSONDecodeError:
-            return JsonResponse({'success': False, 'error': 'Invalid JSON data'}, status=400)
     else:
         return JsonResponse({'error': 'Method not allowed.'}, status=405)
 
 
 @csrf_exempt
 @api_view(['GET', 'PATCH', 'DELETE'])
-def handle_playlist_id(request, pk, cid):
+def handle_playlist_hierarchy_id(request, pk, cid):
     try:
         category = Category.objects.get(pk=pk)
         playlist = Playlist.objects.get(pk=cid, category_id=pk)
     except Category.DoesNotExist:
-        return HttpResponseNotFound("Category not found")
+        return JsonResponse({'success': False, 'error': 'Category not found'}, status=400)
     except Playlist.DoesNotExist:
-        return HttpResponseNotFound("Playlist not found")
+        return JsonResponse({'success': False, 'error': 'Playlist not found'}, status=400)
     except EmptyDataError as e:
         return JsonResponse({'success': False, 'error': str(e)}, status=400)
 
@@ -162,16 +303,83 @@ def handle_playlist_id(request, pk, cid):
         return JsonResponse({'error': 'Method not allowed.'}, status=405)
 
 
+#categories/(?P<pk>\d+)/playlists/(?P<cid>\d+)/songs/
 @api_view(['GET', 'POST'])
-def handle_song(request, pk, cid):
+def handle_song_hierarchy(request, pk, cid):
     try:
         category = Category.objects.get(pk=pk)
         playlist = Playlist.objects.filter(pk=cid, category_id=pk)
     except Category.DoesNotExist:
-        return HttpResponseNotFound("Category not found")
+        return JsonResponse({'success': False, 'error': 'Category not found'}, status=400)
     except Playlist.DoesNotExist:
-        return HttpResponseNotFound("Playlist not found")
+        return JsonResponse({'success': False, 'error': 'Playlist not found'}, status=400)
     return handle_song_basic(cid, request)
+
+
+@csrf_exempt
+@api_view(['GET', 'PATCH', 'DELETE'])
+def handle_song_hierarchy_id(request, pk, cid, tid):
+    try:
+        category = Category.objects.get(pk=pk)
+        playlist = Playlist.objects.get(pk=cid, category_id=pk)
+        song = Song.objects.get(pk=tid, playlist__id=cid)
+    except Category.DoesNotExist:
+        return JsonResponse({'success': False, 'error': 'Category not found'}, status=400)
+    except Playlist.DoesNotExist:
+        return JsonResponse({'success': False, 'error': 'Playlist not found'}, status=400)
+    except Song.DoesNotExist:
+        return JsonResponse({'success': False, 'error': 'Song not found'}, status=400)
+    except EmptyDataError as e:
+        return JsonResponse({'success': False, 'error': str(e)}, status=400)
+    return handle_song_id_basic(request, song)
+
+
+# playlists/(?P<cid>\d+)/songs/
+@api_view(['GET', 'POST'])
+def handle_song_by_playlist(request, cid):
+    try:
+        playlist = Playlist.objects.filter(pk=cid)
+    except Playlist.DoesNotExist:
+        return JsonResponse({'success': False, 'error': 'Playlist not found'}, status=400)
+    return handle_song_basic(cid, request)
+
+
+@csrf_exempt
+@api_view(['GET', 'PATCH', 'DELETE'])
+def handle_song_by_playlist_id(request, cid, tid):
+    try:
+        playlist = Playlist.objects.get(pk=cid)
+        song = Song.objects.get(pk=tid, playlist__id=cid)
+    except Playlist.DoesNotExist:
+        return JsonResponse({'success': False, 'error': 'Playlist not found'}, status=400)
+    except Song.DoesNotExist:
+        return JsonResponse({'success': False, 'error': 'Song not found'}, status=400)
+    except EmptyDataError as e:
+        return JsonResponse({'success': False, 'error': str(e)}, status=400)
+    return handle_song_id_basic(request, song)
+
+
+def handle_song_id_basic(request, song):
+    if request.method == 'GET':
+        serializer = SongSerializer(song)
+        return JsonResponse(serializer.data, safe=False, status=200)
+    elif request.method == 'PATCH':
+        try:
+            data = json.loads(request.body.decode('utf-8'))
+            if not data:
+                return JsonResponse({'success': False, 'error': 'Empty data in PATCH request'}, status=400)
+            serializer = SongSerializer(song, data=data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=200)
+            return JsonResponse(serializer.errors, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'success': False, 'error': 'Invalid JSON data'}, status=400)
+    elif request.method == 'DELETE':
+        song.delete()
+        return HttpResponse(status=204)
+    else:
+        return JsonResponse({'error': 'Method not allowed.'}, status=405)
 
 
 def handle_song_basic(cid, request):
@@ -181,7 +389,7 @@ def handle_song_basic(cid, request):
             serializer = SongSerializer(songs, many=True)
             return JsonResponse(serializer.data, safe=False, status=200)
         except Song.DoesNotExist:
-            return HttpResponseNotFound("Song not found")
+            return JsonResponse({'success': False, 'error': 'Song not found'}, status=400)
         except json.JSONDecodeError:
             return JsonResponse({'success': False, 'error': 'Invalid JSON data'}, status=400)
 
@@ -214,70 +422,5 @@ def handle_song_basic(cid, request):
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except (json.JSONDecodeError, KeyError, Category.DoesNotExist) as e:
             return JsonResponse({'success': False, 'error': str(e)}, status=400)
-    else:
-        return JsonResponse({'error': 'Method not allowed.'}, status=405)
-
-
-@csrf_exempt
-@api_view(['GET', 'PATCH', 'DELETE'])
-def handle_song_id(request, pk, cid, tid):
-    try:
-        category = Category.objects.get(pk=pk)
-        playlist = Playlist.objects.get(pk=cid, category_id=pk)
-        song = Song.objects.get(pk=tid, playlist__id=cid)
-    except Category.DoesNotExist:
-        return HttpResponseNotFound("Category not found")
-    except Playlist.DoesNotExist:
-        return HttpResponseNotFound("Playlist not found")
-    except Song.DoesNotExist:
-        return HttpResponseNotFound("Song not found")
-    except EmptyDataError as e:
-        return JsonResponse({'success': False, 'error': str(e)}, status=400)
-    return handle_song_id_basic(request, song)
-
-
-@api_view(['GET', 'POST'])
-def handle_song_by_playlist(request, cid):
-    try:
-        playlist = Playlist.objects.filter(pk=cid)
-    except Playlist.DoesNotExist:
-        return HttpResponseNotFound("Playlist not found")
-    return handle_song_basic(cid, request)
-
-
-@csrf_exempt
-@api_view(['GET', 'PATCH', 'DELETE'])
-def handle_song_by_playlist_id(request, cid, tid):
-    try:
-        playlist = Playlist.objects.get(pk=cid)
-        song = Song.objects.get(pk=tid, playlist__id=cid)
-    except Playlist.DoesNotExist:
-        return HttpResponseNotFound("Playlist not found")
-    except Song.DoesNotExist:
-        return HttpResponseNotFound("Song not found")
-    except EmptyDataError as e:
-        return JsonResponse({'success': False, 'error': str(e)}, status=400)
-    return handle_song_id_basic(request, song)
-
-
-def handle_song_id_basic(request, song):
-    if request.method == 'GET':
-        serializer = SongSerializer(song)
-        return JsonResponse(serializer.data, safe=False, status=200)
-    elif request.method == 'PATCH':
-        try:
-            data = json.loads(request.body.decode('utf-8'))
-            if not data:
-                return JsonResponse({'success': False, 'error': 'Empty data in PATCH request'}, status=400)
-            serializer = SongSerializer(song, data=data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return JsonResponse(serializer.data, status=200)
-            return JsonResponse(serializer.errors, status=400)
-        except json.JSONDecodeError:
-            return JsonResponse({'success': False, 'error': 'Invalid JSON data'}, status=400)
-    elif request.method == 'DELETE':
-        song.delete()
-        return HttpResponse(status=204)
     else:
         return JsonResponse({'error': 'Method not allowed.'}, status=405)
--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -3,23 +3,22 @@ from django.urls import path, include, re_path
 from rest_framework import routers
 
 from comparer import views
-router = routers.DefaultRouter()
-router.register(r'playlist', views.PlaylistViewSet)
-router.register(r'song', views.SongViewSet)
 
-# todo:
-# postman
-# email
 urlpatterns = [
-    path('', include(router.urls)),
+    re_path(r'^songs/?$', views.handle_song, name='handle_song'),
+    re_path(r'^songs/(?P<pk>\d+)/?$', views.handle_song_id, name='handle_song_id'),
+
+    re_path(r'^playlists/?$', views.handle_playlist, name='handle_playlist'),
+    re_path(r'^playlists/(?P<pk>\d+)/?$', views.handle_playlist_id, name='handle_playlist_id'),
+
     re_path(r'^categories/?$', views.handle_category, name='handle_category'),
     re_path(r'^categories/(?P<pk>\d+)/?$', views.handle_category_id, name='handle_category_id'),
 
-    re_path(r'categories/(?P<pk>\d+)/playlists/?$', views.handle_playlist, name='handle_playlist'),
-    re_path(r'categories/(?P<pk>\d+)/playlists/(?P<cid>\d+)/?$', views.handle_playlist_id, name='handle_playlist_id'),
+    re_path(r'categories/(?P<pk>\d+)/playlists/?$', views.handle_playlist_hierarchy, name='handle_playlist'),
+    re_path(r'categories/(?P<pk>\d+)/playlists/(?P<cid>\d+)/?$', views.handle_playlist_hierarchy_id, name='handle_playlist_id'),
 
-    re_path(r'categories/(?P<pk>\d+)/playlists/(?P<cid>\d+)/songs/?$', views.handle_song, name='handle_song'),
-    re_path(r'categories/(?P<pk>\d+)/playlists/(?P<cid>\d+)/songs/(?P<tid>\d+)/?$', views.handle_song_id,
+    re_path(r'categories/(?P<pk>\d+)/playlists/(?P<cid>\d+)/songs/?$', views.handle_song_hierarchy, name='handle_song'),
+    re_path(r'categories/(?P<pk>\d+)/playlists/(?P<cid>\d+)/songs/(?P<tid>\d+)/?$', views.handle_song_hierarchy_id,
          name='handle_song_id'),
 
     re_path(r'playlists/(?P<cid>\d+)/songs/?$', views.handle_song_by_playlist, name='handle_song'),
